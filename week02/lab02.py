cdca89f3aa2c5f82133c45276f632b68baed61a2
Howard Huang <huang.howard7@gmail.com>
2026-01-20 10:41:27 -0500

    Updated week2 with array and player choice

--- week02/lab02.py
+++ week02/lab02.py
@@ -1,1 +1,8 @@
 # This is the Python file for week 2
+choices = ["Rock", "Paper", "Scissors"]
+test = [1,2,3]
+playerChoice = int(input("Enter a number...\n1 > Rock\n2 > Paper\n3 > Scissors\n"))
+if playerChoice not in [1,2,3]:
+    print("Error: Choice must be between 1 and 3")
+    exit(1)
+
